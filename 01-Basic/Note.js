565f26e6854d406927f30653df287c6c3ea61e76
Zenith-hasan <abuhasan323382@gmail.com>
2025-06-01 14:52:14 +0600

    setup & basic discussion

---
+++ 01-Basic/Note.js
@@ -0,0 +1,24 @@
+//* Express is a minimal and flexible Node.js web application framework that provide a robust set of features for web and mobile application.
+
+//* It is an open source framework developed and maintainded by the Node.js foundation.
+
+//* Express provides a minimal interface to build our application. It provides us the tools that require to build our app.It's flexible as there are numerous modules available on npm,which can be directly plugged into Express.
+
+
+//* 
+
+
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+	res.send(`Hello from NodeJS`)
+})
+
+app.get('/home',(req,res) => {
+	res.send(`This is homepage of our app!`)
+})
+
+app.listen(3000,() => {
+	console.log(`Server listening at https://localhost:3000`)
+})
